74d3e163bd4405a791f372a736b915ce5311579c
Brian de Souza <briandesouza1597@gmail.com>
2024-10-12 10:54:43 -0300

    add rolagem infinita nos produtos

--- src/pages/Products/ProductsPage.jsx
+++ src/pages/Products/ProductsPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, FlatList, Image, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, FlatList, Image, Button, StyleSheet, ActivityIndicator } from 'react-native';
 import { useAuth } from '../../context/AuthContext';
 import { useCart } from '../../context/CartContext';
 
-const products = [
+const initialProducts = [
   { id: '1', name: 'Camisa', price: 29.99, image: 'https://cdn-icons-png.flaticon.com/128/1805/1805035.png' },
   { id: '2', name: 'Camisa', price: 19.99, image: 'https://cdn-icons-png.flaticon.com/128/1805/1805059.png' },
   { id: '3', name: 'Calça', price: 49.99, image: 'https://cdn-icons-png.flaticon.com/128/1811/1811437.png' },
@@ -17,26 +17,28 @@ const products = [
 ];
 
 export const ProductPage = () => {
-  const { addItem } = useCart()
+  const { addItem } = useCart();
 
   const [searchQuery, setSearchQuery] = useState('');
-  const [filteredProducts, setFilteredProducts] = useState(products);
+  const [filteredProducts, setFilteredProducts] = useState(initialProducts);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   const handleSearch = (query) => {
     setSearchQuery(query);
     if (query) {
-      const filteredData = products.filter((product) =>
+      const filteredData = initialProducts.filter((product) =>
         product.name.toLowerCase().includes(query.toLowerCase())
       );
       setFilteredProducts(filteredData);
     } else {
-      setFilteredProducts(products);
+      setFilteredProducts(initialProducts);
     }
   };
 
   function handleAddCart(item) {
-    addItem(item)
-    alert(`${item.name} adicionado ao carrinho!`)
+    addItem(item);
+    alert(`${item.name} adicionado ao carrinho!`);
   }
 
   const renderProduct = ({ item }) => (
@@ -47,6 +49,23 @@ export const ProductPage = () => {
       <Button title="Adicionar ao Carrinho" onPress={() => handleAddCart(item)} />
     </View>
   );
+
+  const loadMoreProducts = () => {
+    if (loading) return; // evitar chamadas duplicadas
+    setLoading(true);
+    
+    // simula uma chamada para carregar mais produtos
+    setTimeout(() => {
+      const newProducts = initialProducts.map((product) => ({
+        ...product,
+        id: `${parseInt(product.id) + page * 10}`, // Gerar IDs únicos
+      }));
+      
+      setFilteredProducts((prev) => [...prev, ...newProducts]);
+      setLoading(false);
+      setPage((prev) => prev + 1); // incrementa página
+    }, 1000);
+  };
 
   return (
     <View style={styles.container}>
@@ -60,6 +79,14 @@ export const ProductPage = () => {
         data={filteredProducts}
         renderItem={renderProduct}
         keyExtractor={(item) => item.id}
+        onEndReached={loadMoreProducts}
+        onEndReachedThreshold={0.4} // Carregar mais produtos quando chegar a 40% do final
+        ListFooterComponent={loading ? ( // componente de mensagem de carregamento ao chegar no final da lista
+          <View style={styles.loadingContainer}> 
+            <ActivityIndicator size="large" color="#5b88a5" />
+            <Text style={styles.loadingText}>Carregando...</Text>
+          </View>
+        ) : null} // mostra o indicador de carregamento
       />
     </View>
   );
@@ -107,5 +134,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#888',
     marginBottom: 8,
+  },
+  loadingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  loadingText: {
+    fontSize: 18,
+    color: '#CCC',
+    marginLeft: 8,
+    fontWeight: 'bold',
   },
 });
